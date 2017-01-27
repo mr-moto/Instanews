fa663c9ceedd4028906ac7707465c89db8343430
mr-moto <khash615@gmail.com>
2017-01-27 15:40:22 -0800

    images working with .filter and .slice method

--- js/main.js
+++ js/main.js
@@ -7,23 +7,16 @@ $(function () {
         dataType: 'json'
     })
 
-        .done(function (data) {
-            if (data.results.length > 0) {
-                $.each(data.results, function (index, value) {
-                    var newsImageLink = '';
-                    //debugger;
-                    if (value.multimedia.length) {
-                        newsImageLink = value.multimedia[4].url;
-                    }
+    .done(function(data){
+        var imgFilter = data.results.filter(function(imgdata){
+            return imgdata.multimedia.length;
+        }).slice(0,12);
+        $.each(imgFilter, function(index, value){
+            var imgLink = '';
+            imgLink = value.multimedia[4].url;
 
-                    $('.grid-list').append('<li class="results-wrap"><div class="h3-wrap"><h3 class="h3result">' + value.abstract + '</h3></div><img class="news-img" src=' + newsImageLink + ' /></li>');
-
-                })
-
-            }
-
-            // else {
-            //     alert('got no data back from server');
-            // }
+            $('.grid-list').append('<li class="results-wrap"><div class="h3-wrap"><h3 class="h3result">' + value.abstract + '</h3></div><img class="news-img" src=' + imgLink + ' /></li>');
         })
+
+    })
 })
