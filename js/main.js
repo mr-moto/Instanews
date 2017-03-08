e4ec5edc31840541cd2697a6d1218cd4283933a4
mr-moto <khash615@gmail.com>
2017-03-07 16:32:52 -0800

    code clean up

--- js/main.js
+++ js/main.js
@@ -2,45 +2,53 @@ $(function () {
 
     $('.loader').hide();
     $('select').selectric();
-        
-    $('select').on('change', function () {
+
+    function toggleSmallHeader() {  
         $('.site-header').addClass('site-header-up').removeClass('site-header');
-        $('.logo').addClass('logo-up').removeClass('logo');
-        $('.logo-wrap').addClass('logo-wrap-up').removeClass('logo-wrap');
+    }
 
-        $('.loader').show();
-
-
-        var $news = $('.grid-list');
-        $news.empty();
+    $('select').on('change', function () {
 
         var category = this.value;
+
+        toggleSmallHeader();
+        $('.loader').show();
+        $('.grid-list').empty();
+
         $.ajax({
             url: 'https://api.nytimes.com/svc/topstories/v2/' + category + '.json?api-key=ce652fdef4f54825ad624cc67075d65b',
             method: 'GET',
             dataType: 'json'
         })
 
-            .done(function (data) { 
-                $('.loader').hide(); 
-                var imgFilter = data.results.filter(function (imgdata) { 
-                    return imgdata.multimedia.length;
-                }).slice(0, 12); 
-                $.each(imgFilter, function (index, value) { 
-                    var imgLink = ''; 
-                    imgLink = value.multimedia[4].url; 
+        .done(function (data) {
+            $('.loader').hide(); 
+            var imgFilter = data.results.filter(function (imgdata) { 
+                return imgdata.multimedia.length;
+            }).slice(0, 12); 
+            $.each(imgFilter, function (index, value) { 
+                var imgLink = ''; 
+                imgLink = value.multimedia[4].url; 
+                
+                var $articlelist = $('.grid-list');
+                var listitem = '';
 
+                listitem += '<li class="article"><a href="'
+                listitem += value.url
+                listitem += '"><img class="news-img" src="'
+                listitem += imgLink
+                listitem += '"/></a><div class="text-wrapper"><p class="article-text">'
+                listitem += value.abstract
+                listitem += '</p></div></li>'
 
-                    $('.grid-list').append('<li class="article"><a href="' + value.url + '"><img class="news-img" src="' + imgLink + '"/></a><div class="text-wrapper"><p class="article-text">' + value.abstract + '</p></div></li>')
-                })
-
+                $articlelist.append(listitem)
             })
-
+        })
     });
+
     $(document).on('click', 'a', function (event) {
         event.preventDefault(); 
         var url = $(this).attr('href'); 
         window.open(url, '_blank');
-
     });
 })
